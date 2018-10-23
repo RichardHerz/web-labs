31f8c4d360254845a014b666cddd09fb6b071775
RichardHerz <rich@reactorlab.net>
2018-10-22 21:34:36 -0700

    edit dev timing display

--- web_labs_JS/process_controller.js
+++ web_labs_JS/process_controller.js
@@ -136,11 +136,10 @@ let controller = {
       elapsedMs = currentMs - startMs;
       updateMs = updateDisplayTimingMs - elapsedMs;
 
-      // // DISPLAY TIMING DATA DURING DEVELOPMENT - PERCENT TIME IDLE
+      // // DISPLAY TIMING DATA DURING DEVELOPMENT
       // // NEED TO EDIT INDEX.HTML TO ACTIVATE "field_output_field"
-      // let tIdleTime = 100*(1-elapsedMs/updateMs);
-      // tIdleTime = Number(tIdleTime).toPrecision(2);
-      // document.getElementById("field_output_field").innerHTML = "% idle = " + tIdleTime + "&nbsp;&nbsp;";
+      // let idleMs = Number(updateMs).toPrecision(2);
+      // document.getElementById("field_output_field").innerHTML = "idle time = " + idleMs + "&nbsp;ms";
 
       // END updateProcess WITH CALL TO ITSELF AFTER updateMs WAIT
       setTimeout(updateProcess, updateMs);  // updateMs

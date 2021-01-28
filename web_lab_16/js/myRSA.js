cbae762f6754a0abb01525f0e2594b2d48ccbd53
RichardHerz <rich@reactorlab.net>
2021-01-27 22:02:48 -0800

    get it working!

--- web_lab_16/js/myRSA.js
+++ web_lab_16/js/myRSA.js
@@ -1,3 +1,100 @@
+function fGetKeys() {
+
+  console.log('enter main function fGetKeys');
+
+  let pf = document.getElementById('input_field_enter_p');
+  let qf = document.getElementById('input_field_enter_q');
+
+  let p = pf.value;
+  let q = qf.value;
+
+  let n = p*q;
+  let phi = (p-1)*(q-1);
+  console.log('phi = ' + phi);
+  let ep = primes(phi);
+  console.log('ep = ' + ep);
+
+  // find a random value in primes that is coprime with phi
+  // this produces different keys every run
+  // if desire same key, let i = 1 or other start
+  // then increment i inside while loop
+  // (or write own random function where can fix seed)
+  let eplen = ep.length;
+  let j = 0; // counter so random pick loop won't run forever
+  let jfac = 5; // factor * eplen to limit random pick loop to run
+  let i = Math.floor(Math.random() * eplen);
+  // let i = 1;
+  while (gcd_two_numbers(ep[i],phi) != 1) {
+    i = Math.floor(Math.random() * eplen);
+    // break out if don't find GCD within loop limit for random pick
+    j++;
+    if (j > jfac * eplen) {
+      console.log('in function fGetKeys, no GCD found');
+      break;
+    }
+    // i = i++;
+    // // break out if start at fixed i and don't find GCD
+    // if (i > ep.length) {
+    //   console.log('in function fGetKeys, no GCD found');
+    //   break;
+    // }
+  }
+  let e = ep[i];
+  console.log('GCD e = ' + e);
+
+  // find modular inverse d of e such that mod(e*d,phi) = 1
+  // use brute force here but check out extended Euclidean algorithm
+  // e.g., at 11:20 of https://youtu.be/Z8M2BTscoD4
+  let d = 1; // initialize
+  while (Math.round(e*d % phi) != 1) {
+      d++;
+  }
+  tm = e*d % phi; // check
+  console.log('d = ' + d + ', check e*d % phi = 1 ?=? ' + tm);
+
+  let pk = [n,e];
+  let sk = [n,d];
+  console.log('pk = ' + pk);
+  console.log('sk = ' + sk);
+
+  // need to return keys or put them in fields
+  document.getElementById('field_public_key_0').innerHTML = pk[0];
+  document.getElementById('field_public_key_1').innerHTML = pk[1];
+  document.getElementById('field_private_key_0').innerHTML = sk[0];
+  document.getElementById('field_private_key_1').innerHTML = sk[1];
+
+  document.getElementById('field_n_value').innerHTML = pk[0];
+
+} // END of function fGetKeys
+
+function fEncode() {
+  let pk0 = document.getElementById('field_public_key_0').innerHTML;
+  let pk1 = document.getElementById('field_public_key_1').innerHTML;
+  let pk = [];
+  pk[0] = Number(pk0); // convert string to number
+  pk[1] = Number(pk1);
+  console.log('in fEncode, pk = ' + pk);
+  let mf = document.getElementById('input_field_enter_numeric_message');
+  let m = mf.value;
+  let c = myRSA_F(m,pk);
+  console.log('in fEncode, c = ' + c);
+  document.getElementById('field_encoded_message').innerHTML = c;
+} // END of function fEncode
+
+function fDecode() {
+  let sk0 = document.getElementById('field_private_key_0').innerHTML;
+  let sk1 = document.getElementById('field_private_key_1').innerHTML;
+  let sk = [];
+  sk[0] = Number(sk0); // convert string to number
+  sk[1] = Number(sk1);
+  console.log('in fDecode, sk = ' + sk);
+  let cf = document.getElementById('field_encoded_message').innerHTML;
+  let c = Number(cf);
+  let m = myRSA_F(c,sk);
+  console.log('in fDecode, m = ' + m);
+  document.getElementById('field_decoded_message').innerHTML = m;
+} // END of function fDecode
+
 function myRSA() {
 
   console.log('enter main function myRSA');

5030a3ce649b5ad0cb1d6cef5f58e49b40ac3468
RichardHerz <rich@reactorlab.net>
2021-06-27 10:11:44 -0700

    update with function fFgene

--- web_lab_000/js/zoo.js
+++ web_lab_000/js/zoo.js
@@ -9,6 +9,18 @@ let data = {
   //           in regular web labs but do I need it here?
   // ANSWER: Yes I do need initialize method: tried without it and doesn't work
   initialize : function() {
+    data.x1 = 10;
+    data.y1 = 10;
+    data.x2 = 100;
+    data.y2 = 100;
+    data.color = 'green';
+    data.width = '4px';
+    data.pi = Math.pi;
+    data.degTOrad = Math.PI / 180;
+    // examples from Lab 00
+    //   angle = angleDeg * degTOrad; // convert user input in degrees to radians
+    //   accel = gravity * Math.sin(-angle); // update accel for angle changes
+
     // data['version'] = new Object();
     // data['version'] = 'TedTok_01';
     // data['target'] = new Object();
@@ -25,7 +37,29 @@ function updateBody() {
 
 function updateDisplay() {
 
-  newSVGline(10,10,100,100,'green','4px','line01');
+  let result = [];
+
+  let a = 30; // angle in degrees
+  let d = 100; // distance, length of line
+  let color = 'green';
+  let width = 3;
+  let id = '';
+  result = fFgene(data.x1,data.y1,a,d,color,width,id);
+  data.x1 = result[0];
+  data.y1 = result[1];
+
+  console.log('result = ' + result);
+  console.log('data.x1 = ' + data.x1);
+  console.log('data.y1 = ' + data.y1);
+
+  a = -45; // angle in degrees
+  d = 50; // distance, length of line
+  color = 'blue';
+  width = 1;
+  id = '';
+  result = fFgene(data.x1,data.y1,a,d,color,width,id);
+  data.x1 = result[0];
+  data.y1 = result[1];
 
 } // END of updateDisplay method
 
@@ -53,4 +87,26 @@ function newSVGline(x1,y1,x2,y2,color,width,id) {
   newLine.setAttribute('id',id);
   $("svg").append(newLine);
 
-}
+} // END OF function newSVGline
+
+function fFgene(x1,y1,a,d,color,width,id) {
+
+   // draw a line of length d at angle a from x,y
+   // angles specified in degrees but JS functions need radians
+   let arad = a * data.degTOrad;
+   let dx = d * Math.cos(arad);
+   let dy = d * Math.sin(arad);
+
+  dx = Math.round(dx);
+  dy = Math.round(dy);
+  let x2 = x1 + dx;
+  let y2 = y1 + dy;
+
+  newSVGline(x1,y1,x2,y2,color,width,id); // ,color,width,id);
+
+  let result = [];
+  result[0] = x2;
+  result[1] = y2;
+  return result
+
+} // END OF function fFgene
